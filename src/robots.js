3a6175b7658e3158de4620f14cf3c97a8b37cafe
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2021-05-26 21:48:02 +0530

    my first basic react based app

---
+++ src/robots.js
@@ -0,0 +1,64 @@
+export const robots = [
+    {
+        id:1,
+        name:'Dheeraj Patil',
+        userName: 'rava',
+        email:'patildheeraj@com'
+
+    },
+    {
+        id:2,
+        name:'Pranay Patil',
+        userName:'pannu',
+        email:'pranaypatil@gmail.com'
+    },
+    {
+        id:3,
+        name:'Manish Patil',
+        userName:'Mannu',
+        email:'manishpatil@gmail.com'
+    },
+    {
+        id:4,
+        name:'Dhanashree Patil',
+        userName:'Dhannu',
+        email:'dhanashreepatil@gmail.com'
+    },
+    {
+        id:5,
+        name:'Divya Patil',
+        userName:'div',
+        email:'divpatil@gmail.com'
+    },
+    {
+        id:6,
+        name:'Daksh Patil',
+        userName:'daku',
+        email:'dakupatil@gmail.com'
+    },
+    {
+        id:7,
+        name:'Sneha Patil',
+        userName:'Sneha',
+        email:'snehapatil@gmail.com'
+    },
+    {
+        id:8,
+        name:'Dhannu Patil',
+        userName:'dannu',
+        email:'dhaapatil@gmail.com'
+    },
+    {
+        id:9,
+        name:'Tejal Patil',
+        userName:'teju',
+        email:'tejupatil@gmail.com'
+    },
+    {
+        id:10,
+        name:'Prajal Patil',
+        userName:'praju',
+        email:'prajupatil@gmail.com'
+    }
+
+];
